57c91e5960200d237866dcca90892186e5cc60af
SymonBezerra <simaobezerratranslate@gmail.com>
2023-01-26 14:44:56 -0300

    TaskRepository impl

---
+++ src/main/java/ifpb/edu/br/atlasboard/repository/TaskRepository.java
@@ -0,0 +1,14 @@
+package ifpb.edu.br.atlasboard.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import ifpb.edu.br.atlasboard.domain.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Long>{
+    
+    @Query("SELECT t FROM Task t JOIN t.user_id u WHERE u = :id")
+    public List<Task> findByUserID (Long id);
+}

a1f0c7b1f6d8d7fc1446573de376119246638ea5
SymonBezerra <simaobezerratranslate@gmail.com>
2023-01-26 14:24:06 -0300

    first commit

---
+++ src/main/java/ifpb/edu/br/atlasboard/AtlasboardApplication.java
@@ -0,0 +1,13 @@
+package ifpb.edu.br.atlasboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifpb/edu/br/atlasboard/AtlasboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ifpb.edu.br.atlasboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

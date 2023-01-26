6a392e22f736b2d7107ac63648512cb16b3ff5be
SymonBezerra <simaobezerratranslate@gmail.com>
2023-01-26 17:28:50 -0300

    findByUser impl on TaskRepository

--- src/main/java/ifpb/edu/br/atlasboard/repository/TaskRepository.java
+++ src/main/java/ifpb/edu/br/atlasboard/repository/TaskRepository.java
@@ -3,14 +3,11 @@ package ifpb.edu.br.atlasboard.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import ifpb.edu.br.atlasboard.domain.Task;
+import ifpb.edu.br.atlasboard.domain.User;
 
 public interface TaskRepository extends JpaRepository<Task, Long>{
     
-    // @Query("SELECT t FROM Task t JOIN t.user_id u WHERE u = :id")
-    // public List<Task> findByUserID (Long id);
-
-    // public List<Task>
+    public List<Task> findByUser (User user);
 }
